ae72629d5850982e8a1d20cffeb6a9b71f8811b4
Adrián Acosa Sánchez <adrianacosa@correo.ugr.es>
2022-09-27 19:21:06 +0200

    Finished component

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-$ = (selector) => document.getElementsByClassName(selector);
+// Function to get the value from a selector given
+const $ = (selector) => document.querySelector(selector)
 
-$('submit').addEventListener('click', () => {
-    let name = $('name').value;
-    $('form').submit();
+const card = $(".card") // Get the card element
+const form = $(".rating") // Get the form element
+const ratings = document.querySelectorAll('input[name="rating-value"]') // Get all the radio values
+const thanks = $(".thanks") // Get the thanks element
+const greeting = $(".greeting") // Get the greeting element
+const valoration = $(".valoration") // Get the valoration element
+
+// We get the thanks div hidden
+thanks.style.display = "none"
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  console.log("Submit!")
+  ratings.forEach((rating) => {
+    if (rating.checked) {
+      greeting.innerHTML = `You selected ${rating.value} out of 5`
+      // Hide valoration and show thanks
+      valoration.style.display = "none"
+      thanks.style.display = "block"
+    }
+  })
 })
